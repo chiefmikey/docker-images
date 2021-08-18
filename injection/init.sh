c74597d8be65edebd32121b9538aea32a5fb90a5
chiefmikey <wolfemikl@gmail.com>
2021-08-18 06:38:21 -0600

    Run cap add in init script

--- injection/init.sh
+++ injection/init.sh
@@ -4,4 +4,5 @@ apk update && apk upgrade
 apk add --no-cache docker sudo
 addgroup root docker
 chmod -R 774 /var/run/docker.sock
+sudo docker run --cap-add NET_ADMIN
 sudo docker exec name-generator_db /cassandra/cqlsh.sh
