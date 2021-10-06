b8e535c79fb502632256a4b77bf8430f3adae756
chiefmikey <wolfemikl@gmail.com>
2021-10-05 23:30:21 -0600

    Use env for command

--- alpine-inject/init.sh
+++ alpine-inject/init.sh
@@ -4,9 +4,5 @@ apk update && apk upgrade
 apk add --no-cache docker sudo
 addgroup root docker
 chmod -R 774 /var/run/docker.sock
-sudo docker exec target sh -c "
-   sed -i 's/Hullo Wurld/Injection Successful/g' /koa/client/public/index.js &&
-   sed -i 's/Index/Injected/g' /koa/client/public/index.html &&
-   echo 'document.getElementById(\"app\").style.color = \"red\";' >> /koa/client/public/index.js
-"
+sudo docker exec target sh -c "$INJECT_COMMAND"
 echo "Target container has been injected"
