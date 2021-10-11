b56e68600b5c8ca4764960a056a3c5fc2c8765d5
chiefmikey <wolfemikl@gmail.com>
2021-10-11 16:11:10 -0600

    Connect to mongoose routes

---
+++ alpine-api/routes/requests/post.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-param-reassign */
+import Router from '@koa/router';
+import { saveUser, savePost } from '../../db/queries/find.js';
+
+const router = new Router({ prefix: '/post' });
+
+router.post('/user', async (ctx) => {
+  try {
+    const results = await saveUser(ctx.request.query.something);
+    ctx.response.status = 200;
+    ctx.response.body = results;
+  } catch (error) {
+    console.error('error with post', error);
+    ctx.response.status = 200;
+  }
+});
+
+router.post('/post', async (ctx) => {
+  try {
+    const results = await savePost(ctx.request.query.something);
+    ctx.response.status = 200;
+    ctx.response.body = results;
+  } catch (error) {
+    console.error('error with post', error);
+    ctx.response.status = 200;
+  }
+});
+
+export default router;
